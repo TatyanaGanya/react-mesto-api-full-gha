6000ba78eae645157fb1a852df88e9aca5e6f08b
TatyanaGanya <121543437+TatyanaGanya@users.noreply.github.com>
2023-09-05 01:55:43 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mongodb' } = process.en
 const app = express();
 
 // cors
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin: 'https://mestofrontent.nomoredomainsicu.ru' }));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
